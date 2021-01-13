e61c896cc8839710aa29a1be752050c79f587ba5
geoffreyadam <geoffrey.adam@ymail.com>
2021-01-13 23:21:04 +0100

    [WIP] Starting to play with api routes / middlewares
     Modifications qui seront validées :
    	nouveau fichier : app/Http/Controllers/AuthController.php
    	modifié :         app/Http/Kernel.php
    	nouveau fichier : app/Http/Middleware/CheckAPIToken.php
    	modifié :         app/Providers/RouteServiceProvider.php
    	modifié :         routes/api.php

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Routing\Controller as BaseController;
+
+class AuthController extends BaseController
+{
+  public function index () 
+  {
+    return 'works';
+  }
+}
