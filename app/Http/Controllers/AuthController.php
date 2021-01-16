dd96238c3ec8f5320a31c4e37376fcfe58aacd20
geoffreyadam <geoffrey.adam@ymail.com>
2021-01-16 23:09:55 +0100

    [FEATURE & UPDATE] Created User model, and migration Added a unique email user validation
     Modifications qui seront validées :
    	modifié :         app/Http/Controllers/AuthController.php
    	modifié :         app/Models/User.php
    	supprimé :        database/factories/UserFactory.php
    	renommé :         database/migrations/2014_10_12_000000_create_users_table.php -> database/migrations/2021_01_16_214738_create_users_table.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -10,15 +11,22 @@ class AuthController extends BaseController
 {
   public function index (Request $request) 
   {
-    $validatedData = Validator::make($request->all(), [
-      'email' => ['required', 'email'],
-      'body' => ['required', 'numeric']
+    $data = $request->all();
+    $validatedData = Validator::make($data, [
+      'email' => ['required', 'email', 'unique:users'],
+      'password' => ['required', 'min:8']
     ]); 
 
     if ($validatedData->fails()) {
-      return json_encode($validatedData->messages());
+      return json_encode(['errors' => $validatedData->messages()]);
     } else {
-      
+      // dump($data['email']);
+      // die;
+      User::create([
+        'email' => $data['email'],
+        'password' => $data['password'],
+      ]);
+      return json_encode(['accepted' => 'Account created']);
     }
   }
 }
